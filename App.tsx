3af7dec1dce44c9afb8c3c3116313f2b45257ca3
William Tracy <william@techtracy.com>
2024-02-21 13:15:05 -0800

    Use expo-dev-client.

--- App.tsx
+++ App.tsx
@@ -9,14 +9,10 @@ import {
 import * as SQLite from 'expo-sqlite';
 
 function App(): React.JSX.Element {
-  const [success, setSuccess] = useState(null);
-  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("Working...");
 
   useEffect(()=> {
     const db = SQLite.openDatabase('database.db');
-    
-    setSuccess(null);
-    setError(null);
     
     db.transactionAsync(async tx => {
       try {
@@ -25,37 +21,17 @@ function App(): React.JSX.Element {
         // Insert a duplicate: THIS SHOULD ERROR
         await tx.executeSqlAsync('INSERT INTO Foo VALUES (?);', [1]);
         // This line never gets reached
-        setSuccess('Success!');
+        setStatus('Success!');
       } catch (err) {
         // The SQL error SHOULD cause this to be executed
-        setError(err);
+        setStatus(String(err));
       }
     }, false);
   }, []);
-
-  function RenderWorking() {
-    if (error == null && success == null)
-        return <Text>Working...</Text>
-    return <View />;
-  }
-
-  function RenderError() {
-    if (error == null)
-      return <View />;
-    return (<Text>{String(error)}</Text>);
-  }
-  
-  function RenderSuccess(){
-    if (success == null)
-      return <View />;
-    return (<Text>{String(success)}</Text>);
-}
   
   return (
     <SafeAreaView>
-        <RenderWorking />
-        <RenderError />
-        <RenderSuccess />
+          <Text>{status}</Text>
     </SafeAreaView>
   );
 }
